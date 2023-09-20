8cad93d424e8c633131194b7a3099371670ed881
Saim Khan <saimkhan065@gmail.com>
2023-09-20 12:08:53 -0400

    services branch first commit

--- aqmsr/src/App.js
+++ aqmsr/src/App.js
@@ -4,20 +4,38 @@ import Logo from "./components/logo/logo";
 import Homenav from "./components/home_NAV/homenav";
 import AboutNav from "./components/about_NAV/aboutNav";
 import Home from "./components/home/home";
+import React, {useState} from "react";
+import About from "./components/about/about";
 
 function App() {
+    const [isHomeClicked, setIsHomeClicked] = useState(true)
+    const [isAboutClicked, setIsAboutClicked] = useState(false)
+    const handleAboutClick = () => {
+        setIsHomeClicked(false)
+        setIsAboutClicked(true)
+    }
+    
+    const handleHomeClick = () => {
+        setIsHomeClicked(true)
+        setIsAboutClicked(false)
+    }
   return (
     <div className="App">
         <div className="Header">
       <Header>
           <Logo/>
-          <Homenav/>
-          <AboutNav/>
+          <div className="App_Header-HomeNav" onClick={handleHomeClick}>
+            <Homenav />
+          </div>
+          
+          <div className="App_Header-AboutNav" onClick={handleAboutClick}>
+            <AboutNav/>
+          </div>
       </Header>
         </div>
-        <>
-            <Home/>
-        </>
+        <div>
+            {isAboutClicked?<About/> : <Home/>}
+        </div>
         
         
         
---
+++ aqmsr/src/components/services_NAV/servicesNAV.js
@@ -0,0 +1,11 @@
+import './servicesNAV.css'
+
+function servicesNAV(){
+    return(
+        <>
+            <h5>Services</h5>
+        </>
+    )
+}
+
+export default servicesNAV()
